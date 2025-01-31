d1d5ce8e3bc06ded8fd025c757395f4eb00e92d7
alexeyzhukov <myshakhovskaya@yandex.ru>
2025-01-31 03:20:32 +0300

    Улучшил внешний вид формы регистрации

--- main/forms.py
+++ main/forms.py
@@ -11,11 +11,19 @@ class OrderForm(forms.ModelForm):
         fields = ('title', 'content', 'price')
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(
+        attrs={'class': 'form-control'}
+    ))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(
+        attrs={'class': 'form-control'}
+    ))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Пароль (повторно)', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}))
 
     captcha = CaptchaField(label='Введите текст с картинки',
-    error_messages={'invalid': 'Неправильный текст'})
+        error_messages={'invalid': 'Неправильный текст'})
 
     class Meta:
         model = User
@@ -24,5 +32,5 @@ class UserRegistrationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.')
+            raise forms.ValidationError('Пароли не совпадают')
         return cd['password2']
