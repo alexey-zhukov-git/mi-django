596b7ee56cbf14f252cf3900682c5ec6f8674b45
alexeyzhukov <myshakhovskaya@yandex.ru>
2025-04-19 21:25:50 +0300

    -

--- main/views.py
+++ main/views.py
@@ -85,7 +85,7 @@ def new_order(request):
             post = form.save(commit=False)
             post.user_id = request.user.id
             post.save()
-            msg = 'На сайте создан новый кейс. Создал пользователь'
+            msg = 'hello'
             send_mail('Django mail', msg, 'mail@microintervals.ru', ['myshakhovskaya@yandex.ru'], fail_silently=False)
             return redirect('/accounts/profile/', pk=post.pk)
     else:
