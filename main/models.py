193338d5e5890f81c47f1d5777600c476676be12
alexeyzhukov <myshakhovskaya@yandex.ru>
2025-04-27 19:47:31 +0300

    Добавил что бюджет в руб

--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=50, verbose_name='Название кейса')
     content = models.TextField(default='', verbose_name='Описание')
-    price = models.IntegerField(default=0, verbose_name='Бюджет')
+    price = models.IntegerField(default=0, verbose_name='Бюджет (руб.)')
     status = models.CharField(verbose_name='status', max_length=50, default='Создано')
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     edited_at = models.DateTimeField(auto_now=True)
